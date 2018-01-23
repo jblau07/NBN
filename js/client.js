ef73515ddf09c31752cec27edffe969ff12c1d65
jblau07 <jlau7248@gmail.com>
2018-01-23 13:48:04 -1000

    basic functionality complete

--- js/client.js
+++ js/client.js
@@ -1,1 +1,16 @@
 const net = require('net');
+
+const client = new net.Socket();
+client.connect(6969, '0.0.0.0', function () {
+  console.log('Connected');
+  process.stdin.pipe(client);
+});
+
+client.on('end', () => {
+  console.log('Disconnected from server')
+
+});
+
+client.on('data', (data) => {
+  console.log(data.toString('utf8'));
+});
--- js/server.js
+++ js/server.js
@@ -1,1 +1,32 @@
 const net = require('net');
+
+let connections = [];
+
+const server = net.createServer((socket) => {
+  console.log('Client connected');
+  socket.on('end', () => {
+    console.log('Client disconnected');
+  });
+  socket.write('You are connected\r\n');
+  connections.push(socket);
+
+  socket.on('data', (chunk) => {
+    socket.setEncoding('utf8');
+    console.log(chunk.toString('utf8'));
+
+    connections.filter(element => {
+        return element !== socket;
+      })
+      .forEach(element => {
+        element.setEncoding('utf8');
+        element.write(chunk)
+      })
+  });
+})
+
+server.on('error', (err) => {
+  throw err;
+});
+server.listen(6969, '0.0.0.0', () => {
+  console.log('server bound');
+});
